e29332e5ae72653d799f03bf0009a546fe6e817e
zhangyanchao <zhangyanchao@yoozoo.com>
2021-07-23 18:32:14 +0800

    20210723 Recall

--- src/main/java/com/std/arithemic/sort/TestSort.java
+++ src/main/java/com/std/arithemic/sort/TestSort.java
@@ -6,106 +6,74 @@ import java.util.Queue;
 
 /**
  * @author zyc
- * @date 2021/6/18 16:51
+ * @date 2021/7/23 10:28
  */
 public class TestSort {
 
     public static void main(String[] args) {
-        int[] nums = {123,4,34,7,12,45,22};
-//        bubbleSort(nums);
-//        selectSort(nums);
-//        insertSort(nums);
-//        bucketSort(nums);
+        int[] nums = {63,129,30,2,24,88,99,17};
+//        bubbleSort (nums);
+//        insertSort (nums);
+//        selectSort (nums);
+//        mergeSort (nums);
+//        bucketSort (nums);
 //        quickSort(nums);
-//        heapSort(nums);
-        mergeSort(nums);
+        heapSort(nums);
         System.out.println(Arrays.toString(nums));
     }
 
-    private static void mergeSort (int[] nums) {
-        mergeSort(nums, 0, nums.length-1);
-    }
-    private static void mergeSort (int[] nums, int start, int end) {
-        if (end > start) {
-            int middleIndex = start + (end - start) / 2;
-            mergeSort(nums, start, middleIndex);
-            mergeSort(nums, middleIndex + 1, end);
-            mergeSort(nums, start, middleIndex, end);
-        }
-    }
-    private static void mergeSort(int[] arr, int low, int middle, int high) {
-        int[] temp = new int[high - low + 1];
-        int start = low, middleIndex = middle + 1, tempIndex = 0;
-        while (start <= middleIndex && middleIndex <= high) {
-            if (arr[start] > arr[middleIndex]) {
-                temp[tempIndex++] = arr[middleIndex++];
-            } else {
-                temp[tempIndex++] = arr[start++];
-            }
-        }
-        while (start <= middleIndex) {
-            temp[tempIndex++] = arr[start++];
-        }
-        while (middleIndex <= high) {
-            temp[tempIndex++] = arr[middleIndex++];
-        }
-        for (int i = 0; i < temp.length; i++) {
-            arr[i+low] = temp[i];
-        }
-    }
-
-    private static void heapSort (int[] nums) {
+    private static void heapSort(int[] nums) {
         buildHeap(nums);
         for (int i = nums.length - 1; i > 0; i--) {
-            swap(nums, 0, i);
+            swapNum(nums, i, 0);
             theHeap(nums, i, 0);
         }
     }
     private static void buildHeap(int[] nums) {
-        int index = (nums.length-2)/2;
-        for (int i = index ; i >= 0 ; i--) {
+        int endNodeIndex = (nums.length-2) >> 1;
+        for (int i = endNodeIndex; i >= 0; i--) {
             theHeap(nums, nums.length, i);
         }
     }
-    private static void theHeap(int[] nums,int size, int index) {
-        int leftChild = index * 2 + 1;
-        int rightChild = index * 2 + 2;
-        int maxIndex = index;
-        if (leftChild < size &&  nums[leftChild] > nums[maxIndex]){
-            maxIndex = leftChild;
+    private static void theHeap(int[] nums, int numsSize, int nodeIndex) {
+        int leftChildInx = (nodeIndex << 1) + 1,
+            rightChildInx = (nodeIndex << 1) + 2,
+            maxIndex = nodeIndex;
+        if (leftChildInx < numsSize && nums[leftChildInx] > nums[maxIndex]) {
+            maxIndex = leftChildInx;
         }
-        if (rightChild < size && nums[rightChild] > nums[maxIndex]) {
-            maxIndex = rightChild;
+        if (rightChildInx < numsSize && nums[rightChildInx] > nums[maxIndex]) {
+            maxIndex = rightChildInx;
         }
-        if (maxIndex != index) {
-            swap(nums, maxIndex, index);
-            theHeap(nums, size, maxIndex);
+        if (maxIndex != nodeIndex) {
+            swapNum(nums, maxIndex, nodeIndex);
+            theHeap(nums, numsSize, maxIndex);
         }
     }
 
     private static void quickSort(int[] nums) {
         quickSort(nums, 0, nums.length-1);
     }
-    private static void quickSort(int[] nums, int start, int end) {
-        int standNum = nums[start];
-        int low = start;
-        int high = end;
-        while (high>low) {
-            while (high>low && nums[high] > standNum) {
-                high--;
+    private static void quickSort(int[] nums, int startIndex, int endIndex) {
+        if (endIndex > startIndex){
+            int standNum = nums[startIndex], lowIndex = startIndex, highIndex = endIndex;
+            while (highIndex > lowIndex) {
+                while (highIndex > lowIndex && nums[highIndex] >= standNum) {
+                    highIndex --;
+                }
+                nums[lowIndex] = nums[highIndex];
+                while (highIndex > lowIndex && nums[lowIndex] <= standNum) {
+                    lowIndex ++;
+                }
+                nums[highIndex] = nums[lowIndex];
             }
-            nums[low] = nums[high];
-            while (high>low && nums[low] < standNum) {
-                low ++;
-            }
-            nums[high] = nums[low];
+            nums[lowIndex] = standNum;
+            quickSort(nums, startIndex, lowIndex - 1);
+            quickSort(nums, lowIndex + 1, endIndex);
         }
-        nums[low] = standNum;
-        quickSort(nums, start, low - 1);
-        quickSort(nums, low + 1, end);
     }
 
-    private static void bucketSort (int[] nums) {
+    private static void bucketSort(int[] nums) {
         Queue<Integer>[] queues = new LinkedList[10];
         for (int i = 0; i < queues.length; i++) {
             queues[i] = new LinkedList<>();
@@ -116,66 +84,96 @@ public class TestSort {
                 maxValue = num;
             }
         }
-        int maxValueLength = String.valueOf(maxValue).length();
-        for (int i = 0; i < maxValueLength; i++) {
-            for(int num : nums) {
+        int valLength = String.valueOf(maxValue).length();
+        for (int i = 0; i < valLength; i++) {
+            for (int num : nums) {
                 int queueIndex = (num/(int)Math.pow(10, i))%10;
                 queues[queueIndex].offer(num);
             }
-            int arrIndex = 0;
+            int numIndex = 0;
             for (Queue<Integer> queue : queues) {
                 while (!queue.isEmpty()) {
-                    nums[arrIndex++] = queue.poll();
+                    nums[numIndex ++] = queue.poll();
                 }
             }
         }
     }
 
-    private static void insertSort (int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int temp = nums[i];
-            int j = i -1;
-            while (j >=0 && nums[j] > temp) {
-                nums[j+1] = nums[j];
-                j--;
+    private static void mergeSort(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+    }
+    private static void mergeSort(int[] nums, int startIndex, int endIndex) {
+        if (endIndex > startIndex) {
+            int middleIndex = startIndex + ((endIndex - startIndex) >> 1);
+            mergeSort(nums, startIndex, middleIndex);
+            mergeSort(nums, middleIndex + 1, endIndex);
+            mergeSort(nums, startIndex, middleIndex, endIndex);
+        }
+    }
+    private static void mergeSort(int[] nums, int startIndex, int middleIndex, int endIndex) {
+        int[] temp = new int[endIndex - startIndex + 1];
+        int lowIndex = startIndex, highIndex = middleIndex + 1, tempIndex = 0;
+        while (lowIndex <= middleIndex && highIndex <= endIndex) {
+            if (nums[lowIndex] < nums[highIndex]) {
+                temp[tempIndex++] = nums[lowIndex++];
+            } else {
+                temp[tempIndex++] = nums[highIndex++];
             }
-            nums[j+1] = temp;
+        }
+        while (lowIndex <= middleIndex) {
+            temp[tempIndex++] = nums[lowIndex++];
+        }
+        while (highIndex <= endIndex) {
+            temp[tempIndex++] = nums[highIndex++];
+        }
+        for (int i = 0; i < temp.length; i++) {
+            nums[i + startIndex] = temp[i];
         }
     }
 
-    private static void selectSort (int[] nums) {
-        for (int i = 0; i < nums.length-1; i++) {
+    private static void selectSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
             int minIndex = i;
-            for (int j = 1+i; j < nums.length; j++) {
-                if (nums[minIndex]> nums[j]){
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
                     minIndex = j;
                 }
             }
-            if (minIndex!=i) {
-                swap(nums, minIndex,i);
+            if (minIndex != i) {
+                swapNum(nums, minIndex, i);
             }
-
         }
     }
 
-    private static void bubbleSort (int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            boolean flag = true;
-            for (int j = 0; j < nums.length-i -1; j++) {
-                if (nums[j] > nums[j+1]) {
-                    swap(nums, j, j+1);
-                }
-                flag = false;
+    private static void insertSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int temp = nums[i];
+            int j = i - 1;
+            while (j >=0 && nums[j] > temp) {
+                nums[j + 1] = nums[j--];
             }
-            if (flag) {
+            nums[j + 1] = temp;
+        }
+    }
+
+    private static void bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            boolean isSwap = true;
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swapNum(nums, j, j + 1);
+                    isSwap = false;
+                }
+            }
+            if (isSwap) {
                 break;
             }
         }
-
     }
-    private static void swap (int[] nums, int source, int target) {
-        int temp = nums[source];
-        nums[source] = nums[target];
-        nums[target] = temp;
+
+    private static void swapNum (int[] nums, int from, int to) {
+        int temp = nums[from];
+        nums[from] = nums[to];
+        nums[to] = temp;
     }
 }
